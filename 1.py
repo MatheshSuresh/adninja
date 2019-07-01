c6a6e444a786a2419054c15d336f2be43ec53ce7
Mathesh Suresh <33441626+MatheshSuresh@users.noreply.github.com>
2019-07-01 15:33:19 +0100

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,41 @@
+import firebase_admin
+import google.cloud
+import json
+from firebase_admin import credentials, firestore
+
+def firebase(): 
+    cred = credentials.Certificate("./ServiceAccountKey.json")
+    app = firebase_admin.initialize_app(cred)
+
+    store = firestore.client()
+
+    users_ref = store.collection(u'Coimbatore').document(u'01')
+	
+    try:           
+        user = users_ref.get({u'media01'})
+        sample_data=(u'{}'.format(user.to_dict()))
+        f= True
+        link = ''
+        while (f==True):
+        for i in range(13,999):
+            try:
+                link = link + sample_data[i]    
+            except IndexError:
+                f= False
+                destination = ((link[:-2]))
+                download(destination)
+                break
+    except google.cloud.exceptions.NotFound:
+        print(u'No such document!')
+
+def download(url):
+    import urllib.request
+    print(url)
+    urllib.request.urlretrieve(url,'1.mp4')
+
+def main():
+        firebase()
+
+if __name__ == '__main__':
+    main()
+    
